1a3749fb595e69c5df89850ceb712456531b9e66
Michał Klepner <michal.klepner1@o2.pl>
2024-11-12 10:50:24 +0100

    Added generateStaticParams for static pages

--- app/cabins/[cabinId]/not-found.js
+++ app/cabins/[cabinId]/not-found.js
@@ -4,13 +4,13 @@ function NotFound() {
   return (
     <main className="text-center space-y-6 mt-4">
       <h1 className="text-3xl font-semibold">
-        This cabin could not be found :(
+        This cabin could not be found :/
       </h1>
       <Link
         href="/"
         className="inline-block bg-accent-500 text-primary-800 px-6 py-3 text-lg"
       >
-        Back to cabin's page
+        Back to cabin page
       </Link>
     </main>
   );
