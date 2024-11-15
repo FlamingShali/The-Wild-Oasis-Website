aa8885aa27e479b25264d40197d6b80ffc1495a4
Michał Klepner <michal.klepner1@o2.pl>
2024-11-15 14:00:07 +0100

    Added updating profile feature

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,6 +1,29 @@
 "use server";
 
-import { signIn, signOut } from "./auth";
+import { auth, signIn, signOut } from "./auth";
+import { supabase } from "./supabase";
+
+export async function updateGuest(formData) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  const nationalID = formData.get("nationalID");
+  const [nationality, countryFlag] = formData.get("nationality").split("%");
+
+  if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID))
+    throw new Error("Please provide a valid national ID");
+
+  const updateData = { nationality, countryFlag, nationalID };
+
+  const { data, error } = await supabase
+    .from("guests")
+    .update(updateData)
+    .eq("id", session.user.guestId)
+
+  if (error) throw new Error("Guest could not be updated");
+
+  console.log(updateData);
+}
 
 export async function singInAction() {
   await signIn("google", { redirectTo: "/account" });
@@ -9,4 +32,3 @@ export async function singInAction() {
 export async function signOutAction() {
   await signOut({ redirectTo: "/" });
 }
-
